00dadd4fafa6513cebd84abf5eb5f5e014912546
Fred Chien <fred@mandice.com>
2011-09-04 01:26:02 +0800

    Clear unnecessary code

--- src/widget.c
+++ src/widget.c
@@ -13,17 +13,6 @@
 #include "widget.h"
 #include "module.h"
 
-static gboolean
-jsh_widget_focus_cb(ClutterActor *actor, ClutterEvent *event, gpointer data)
-{
-	JshWidget *widget = (JshWidget *)data;
-	JshShelf *shelf = (JshShelf *)widget->parent;
-
-	clutter_actor_raise_top(widget->container);
-
-	return TRUE;
-}
-
 JshWidget *
 jsh_widget_new(JuShelf *jushelf, const gchar *module_name)
 {
@@ -41,14 +30,7 @@ jsh_widget_init(JuShelf *jushelf, JshShelf *shelf, JshWidget *widget, JsonNode *
 {
 	widget->parent = (gpointer)shelf;
 	widget->container = clutter_group_new();
-//	clutter_actor_set_anchor_point_from_gravity(widget->container, CLUTTER_GRAVITY_CENTER);
 
-	/* Got focus and raise */
-/*
-	clutter_actor_set_reactive(widget->container, TRUE);
-	g_signal_connect(widget->container, "enter-event",
-		G_CALLBACK(jsh_widget_focus_cb), widget);
-*/
 	jsh_module_constructor(widget, node);
 }
 
